332950da916bc08ed618f9891ca30e06d6efe09a
ElBernie <arroues.bernard@gmail.com>
2023-08-22 18:16:50 +0200

    chore:removing unused import

--- src/app/account/signin/page.tsx
+++ src/app/account/signin/page.tsx
@@ -1,6 +1,6 @@
 import AuthForm from '@/components/_forms/auth.form';
 import { getServerSession } from 'next-auth';
-import { useSession } from 'next-auth/react';
+
 import { redirect } from 'next/navigation';
 
 const AuthPage = async () => {
