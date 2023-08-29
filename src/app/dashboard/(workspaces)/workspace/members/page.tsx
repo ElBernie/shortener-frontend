df60b55bf0ab0ff3e9be4954cd41cfb7dd9878f0
ElBernie <arroues.bernard@gmail.com>
2023-08-29 13:37:29 +0200

    refactor(workspace): check if workspace is a personnal workspace

--- src/app/dashboard/(workspaces)/workspace/members/page.tsx
+++ src/app/dashboard/(workspaces)/workspace/members/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 const WorkspaceMembersPage = async () => {
 	const session = await getServerSession(authOptions);
 	if (!session?.currentWorkspace) redirect('/dashboard/workspaces');
-
+	if (session.currentWorkspace.type === 'PERSONAL') redirect('/dashboard');
 	const membersRequest = await fetch(
 		`${process.env.API_URL}/workspaces/${session?.currentWorkspace.id}/members`
 	);
