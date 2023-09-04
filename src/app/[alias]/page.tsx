32118c9bc06507adf5d14b111143efe218ee9e6c
ElBernie <arroues.bernard@gmail.com>
2023-09-04 10:36:26 +0200

    feat(links): handle password protected links

--- src/app/[alias]/page.tsx
+++ src/app/[alias]/page.tsx
@@ -1,15 +1,52 @@
+import validateLinkPassword from '@/actions/validate-password.action';
 import { notFound, redirect } from 'next/navigation';
-const AliasPage = async ({ params }: { params: { alias: string } }) => {
-	const data = await fetch(`${process.env.API_URL}/links/alias/${params.alias}`, {
-		cache: 'no-cache',
-	});
+import { headers } from 'next/headers';
 
-	if (data.status == 404) throw notFound();
+const AliasPage = async ({
+	params,
+	searchParams,
+}: {
+	params: { alias: string };
+	searchParams?: {
+		[key: string]: string | string[] | undefined;
+	};
+}) => {
+	const linkRequest = await fetch(
+		`${process.env.API_URL}/links/alias/${params.alias}`,
+		{
+			cache: 'no-cache',
+		}
+	);
+
+	if (!linkRequest.ok) throw new Error('Something went wrong');
+	if (linkRequest.status == 404) throw notFound();
 	/**@todo better error handling */
-	if (data.status != 200) throw new Error('Something went wrong');
 
-	const json = await data.json();
-	return redirect(json.URL.url);
+	const data = await linkRequest.json();
+	if (data.password) {
+		return (
+			<div>
+				Password protected
+				<form
+					action={async (formData) => {
+						'use server';
+						validateLinkPassword({
+							userInput: formData,
+							linkData: data,
+							onSuccess: () => redirect(data.URL.url),
+							onError: () =>
+								redirect(headers().get('next-url') + '/?error=wrong-password'),
+						});
+					}}
+				>
+					<input type='password' name='linkPassword' />
+					<button type='submit'>Submit</button>
+				</form>
+				{searchParams?.error && <p>{searchParams.error}</p>}
+			</div>
+		);
+	}
+	return redirect(data.URL.url);
 };
 
 export default AliasPage;
