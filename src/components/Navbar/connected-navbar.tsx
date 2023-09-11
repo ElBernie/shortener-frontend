4ecdfeabb81e554cb6bd9e2ebf79074469cacbe3
ElBernie <arroues.bernard@gmail.com>
2023-09-11 15:26:35 +0200

    feat(navbar): add a link to switch workspace on mobile nav

--- src/components/Navbar/connected-navbar.tsx
+++ src/components/Navbar/connected-navbar.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import style from './style.module.scss';
 import Link from 'next/link';
 import WorkspaceSelector from '../WorkspaceSelector';
 import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const ConnectedNavbar = () => {
 	const session = useSession();
 	const [navbarOpen, setNavbarOpen] = useState(false);
+
 	return (
 		<>
 			<button
@@ -27,17 +29,37 @@ const ConnectedNavbar = () => {
 							<span>Workspace</span>
 							<ul>
 								<li>
-									<Link href='/dashboard/workspace'>Workspace</Link>
+									<Link
+										href='/dashboard/workspace'
+										onClick={() => setNavbarOpen(false)}
+									>
+										Accueil
+									</Link>
 								</li>
 								<li>
-									<Link href='/dashboard/workspace/links'>Liens</Link>
+									<Link
+										href='/dashboard/workspace/links'
+										onClick={() => setNavbarOpen(false)}
+									>
+										Liens
+									</Link>
 								</li>
 								{session?.data?.currentWorkspace?.type === 'PROFESSIONAL' && (
-									<li>Members</li>
+									<li>
+										<Link
+											href='/dashboard/workspace/members'
+											onClick={() => setNavbarOpen(false)}
+										>
+											Members
+										</Link>
+									</li>
 								)}
 							</ul>
 						</>
 					)}
+				</div>
+				<div>
+					<Link href='/dashboard/workspaces'>Switch workspace</Link>
 				</div>
 			</nav>
 			<nav className={`${style.navbar}`}>
