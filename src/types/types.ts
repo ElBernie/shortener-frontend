1bfc0780087c0bd5b1aa5095daad05a28ba6cbb9
ElBernie <arroues.bernard@gmail.com>
2023-09-11 16:26:41 +0200

    refactor: remove unwanted comment

--- src/types/types.ts
+++ src/types/types.ts
@@ -25,23 +25,7 @@ export type Workspace = {
 	permissions?: typeof WORKSPACE_PERMISSIONS;
 };
 
-/**
- * id": "2a9b61d7-0bbb-4835-b31c-ac20559854ca",
-        "email": "arroues.bernard+4@gmail.com",
-        "createdAt": "2023-08-29T14:15:25.747Z",
-        "updatedAt": "2023-08-29T14:15:25.747Z",
-        "workspaceId": "c6bd86e4-f981-42b8-93da-bd4eee5e8079",
-        "workspace": {
-            "id": "c6bd86e4-f981-42b8-93da-bd4eee5e8079",
-            "name": "Workspace perso",
-            "ownerId": "035dfdb7-f067-45d8-bf81-1a9700d649ca",
-            "type": "PERSONAL",
-            "deletable": false,
-            "createdAt": "2023-08-29T12:18:56.955Z",
-            "updatedAt": "2023-08-29T12:18:39.950Z"
-        }
-    }
- */
+
 export type Invite = {
 	id: string;
 	email: string;
