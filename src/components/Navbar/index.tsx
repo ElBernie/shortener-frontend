77aec4f7729d6ac5c9e22cda528b9c511b61b2d2
ElBernie <arroues.bernard@gmail.com>
2023-09-12 16:54:23 +0200

    style:improve design for navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,12 +3,18 @@ import { getServerSession } from 'next-auth';
 import ConnectedNavbar from './connected-navbar';
 import style from './style.module.scss';
 import NavbarUnconnected from './unconnected-navbar';
-
+import Link from 'next/link';
+import { Poppins } from 'next/font/google';
+const poppins = Poppins({ weight: ['700'], subsets: ['latin-ext'] });
 const Navbar = async () => {
 	const session = await getServerSession(authOptions);
 	return (
 		<header className={style.header}>
-			<span className={style.logo}>{process.env.NEXT_APP_NAME || 'Shrt'}</span>
+			<span className={`${style.logo} ${poppins.className}`}>
+				<Link href='/' style={{ all: 'inherit' }}>
+					{process.env.NEXT_APP_NAME || 'Shrt'}
+				</Link>
+			</span>
 
 			{session ? <ConnectedNavbar /> : <NavbarUnconnected />}
 		</header>
