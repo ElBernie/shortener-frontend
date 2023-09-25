37f5ffbec2f7f66c04291657499256653e9ebade
ElBernie <arroues.bernard@gmail.com>
2023-09-25 17:03:23 +0200

    feat(users):add an action to delete an user

---
+++ src/actions/users/deleteUser.action.ts
@@ -0,0 +1,24 @@
+'user server';
+
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+
+export const DeleteUser = async (userId: string) => {
+	const session = await getServerSession(authOptions);
+	if (!session?.user.accessToken) throw new Error('UNAUTHENTICATED');
+
+	const deleteUserRequest = await fetch(
+		`${process.env.API_URL}/users/${userId}`,
+		{
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${session.user.accessToken}`,
+			},
+		}
+	);
+
+	/** @todo better handling */
+	if (!deleteUserRequest.ok) throw new Error();
+	return true;
+};
